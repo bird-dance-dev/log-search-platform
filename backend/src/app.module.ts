f7db16aa1f8826bdef921b736332d04a3b1aecc4
bird-dance <bird.dance.engine@gmail.com>
2026-06-06 17:21:50 +0900

    ヘルスチェックエンドポイントに変更

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +8,5 @@ import { SettingsModule } from './settings/settings.module';
 @Module({
   imports: [PrismaModule, EventsModule, AuthModule, SettingsModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
